c559b688518c7f1b3ff47864252490e03072929d
Zinger88 <kylinar88@gmail.com>
2018-05-16 22:39:40 +0300

    first of

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+
+class App extends Component {
+  render() {
+    return <h1>lesson time 1.38 <a href='https://www.youtube.com/watch?v=3rq4b6Ozjf8&t=0s&list=LLdXKS_587dlwhVdI_48OdRg&index=1'> Link for lesson</a></h1>;
+  }
+}
+
+export default App;
